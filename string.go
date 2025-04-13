4982ba62458f492edeb2fdbdc35a26a21def4231
gram <git@orsinium.dev>
2025-04-13 07:25:48 +0200

    String.Replace

--- string.go
+++ string.go
@@ -3,6 +3,7 @@ package regexer
 import (
 	"iter"
 	"regexp"
+	"strings"
 )
 
 type (
@@ -49,6 +50,49 @@ func (b String) Find() iter.Seq[SMatch] {
 	}
 }
 
+func (s String) Replace(res *string) iter.Seq[SReplacement] {
+	return func(yield func(SReplacement) bool) {
+		prevEnd := 0
+		resBuilder := strings.Builder{}
+		for match := range s.Find() {
+			resBuilder.WriteString(s.src[prevEnd:match.Span.Start])
+			ok := yield(SReplacement{
+				Match:  match,
+				rex:    s.rex,
+				src:    s.src[prevEnd:],
+				result: &resBuilder,
+			})
+			if !ok {
+				return
+			}
+			prevEnd = match.Span.End
+		}
+		resBuilder.WriteString(s.src[prevEnd:])
+		*res = resBuilder.String()
+	}
+}
+
 func (b String) Contains() bool {
 	return b.rex.MatchString(b.src)
+}
+
+type SReplacement struct {
+	Match[string]
+	rex    *regexp.Regexp
+	src    string
+	result *strings.Builder
+}
+
+func (r SReplacement) ReplaceLiteral(val string) {
+	r.result.WriteString(val)
+}
+
+func (r SReplacement) ReplaceTemplate(val string) {
+	// TODO: avoid allocations on bytes<->string conversion by using unsafe.
+	suffix := r.rex.Expand(nil, []byte(val), []byte(r.src), r.Subs.rawSpans)
+	r.result.WriteString(string(suffix))
+}
+
+func (r SReplacement) ReplaceFunc(f func(string) string) {
+	r.result.WriteString(f(r.Match.Content))
 }
